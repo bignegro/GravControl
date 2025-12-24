69da6adb0b913daa4ab0ac265c64e02be4cc1f6d
bignegro <charlene302@hotmail.com>
2025-12-24 01:59:55 -0500

    Refactor GetGravityUp function for clarity and efficiency

--- Loader.lua
+++ Loader.lua
@@ -29,94 +29,85 @@ local FEELER_APEX_OFFSET = 1
 local FEELER_WEIGHTING = 8
 
 function GetGravityUp(self, oldGravityUp)
-	local ignoreList = {}
-	for i, player in next, PLAYERS:GetPlayers() do
-		ignoreList[i] = player.Character
-	end
-
-	-- get the normal
-
 	local hrpCF = self.HRP.CFrame
 	local isR15 = (self.Humanoid.RigType == Enum.HumanoidRigType.R15)
 
-	local origin = isR15 and hrpCF.p or hrpCF.p + 0.35*oldGravityUp
-	local radialVector = math.abs(hrpCF.LookVector:Dot(oldGravityUp)) < 0.999 and hrpCF.LookVector:Cross(oldGravityUp) or hrpCF.RightVector:Cross(oldGravityUp)
+	local origin = isR15 and hrpCF.Position or hrpCF.Position + 0.35 * oldGravityUp
+	local radialVector =
+		math.abs(hrpCF.LookVector:Dot(oldGravityUp)) < 0.999
+		and hrpCF.LookVector:Cross(oldGravityUp)
+		or hrpCF.RightVector:Cross(oldGravityUp)
 
 	local centerRayLength = 25
 	local centerRay = Ray.new(origin, -centerRayLength * oldGravityUp)
-	local centerHit, centerHitPoint, centerHitNormal = workspace:FindPartOnRayWithIgnoreList(centerRay, {LP.Character})
 
-	--DrawClass:Clear()
-	--DrawClass.Draw3D.Ray(centerRay.Origin, centerRay.Direction)
-
-	local downHitCount = 0
-	local totalHitCount = 0
-	local centerRayHitCount = 0
-	local evenRayHitCount = 0
-	local oddRayHitCount = 0
+	local centerHit, _, centerHitNormal =
+		workspace:FindPartOnRayWithIgnoreList(centerRay, { LP.Character or workspace })
 
 	local mainDownNormal = ZERO
-	if (centerHit) then
+	if centerHit then
 		mainDownNormal = centerHitNormal
-		centerRayHitCount = 0
 	end
 
 	local downRaySum = ZERO
-	for i = 1, NUM_DOWN_RAYS do
-		local dtheta = PI2 * ((i-1)/NUM_DOWN_RAYS)
+	local downHitCount = 0
 
+	for i = 1, NUM_DOWN_RAYS do
+		local dtheta = PI2 * ((i - 1) / NUM_DOWN_RAYS)
 		local angleWeight = 0.25 + 0.75 * math.abs(math.cos(dtheta))
-		local isEvenRay = (i%2 == 0)
-		local startRadius = isEvenRay and EVEN_DOWN_RAY_START_RADIUS or ODD_DOWN_RAY_START_RADIUS	
+		local isEvenRay = (i % 2 == 0)
+
+		local startRadius = isEvenRay and EVEN_DOWN_RAY_START_RADIUS or ODD_DOWN_RAY_START_RADIUS
 		local endRadius = isEvenRay and EVEN_DOWN_RAY_END_RADIUS or ODD_DOWN_RAY_END_RADIUS
-		local downRayLength = centerRayLength
 
 		local offset = CFrame.fromAxisAngle(oldGravityUp, dtheta) * radialVector
-		local dir = (LOWER_RADIUS_OFFSET * -oldGravityUp + (endRadius - startRadius) * offset)
-		local ray = Ray.new(origin + startRadius * offset, downRayLength * dir.unit)
-		local hit, hitPoint, hitNormal = workspace:FindPartOnRayWithIgnoreList(ray, {LP.Character})
+		local dir = (LOWER_RADIUS_OFFSET * -oldGravityUp + (endRadius - startRadius) * offset).Unit
 
-		--DrawClass.Draw3D.Ray(ray.Origin, ray.Direction)
+		local ray = Ray.new(
+			origin + startRadius * offset,
+			centerRayLength * dir
+		)
 
-		if (hit) then
-			downRaySum = downRaySum + angleWeight * hitNormal
-			downHitCount = downHitCount + 1
-			if isEvenRay then
-				evenRayHitCount = evenRayHitCount + 1					
-			else
-				oddRayHitCount = oddRayHitCount + 1
-			end
+		local hit, _, hitNormal =
+			workspace:FindPartOnRayWithIgnoreList(ray, { LP.Character or workspace })
+
+		if hit then
+			downRaySum += angleWeight * hitNormal
+			downHitCount += 1
 		end
 	end
 
-	local feelerHitCount = 0	
 	local feelerNormalSum = ZERO
+	local feelerHitCount = 0
 
 	for i = 1, NUM_FEELER_RAYS do
-		local dtheta = 2 * math.pi * ((i-1)/NUM_FEELER_RAYS)
-		local angleWeight =  0.25 + 0.75 * math.abs(math.cos(dtheta))	
+		local dtheta = PI2 * ((i - 1) / NUM_FEELER_RAYS)
+		local angleWeight = 0.25 + 0.75 * math.abs(math.cos(dtheta))
 		local offset = CFrame.fromAxisAngle(oldGravityUp, dtheta) * radialVector
-		local dir = (FEELER_RADIUS * offset + LOWER_RADIUS_OFFSET * -oldGravityUp).unit
-		local feelerOrigin = origin - FEELER_APEX_OFFSET * -oldGravityUp + FEELER_START_OFFSET * dir
+
+		local dir = (FEELER_RADIUS * offset + LOWER_RADIUS_OFFSET * -oldGravityUp).Unit
+		local feelerOrigin = origin + FEELER_APEX_OFFSET * oldGravityUp + FEELER_START_OFFSET * dir
+
 		local ray = Ray.new(feelerOrigin, FEELER_LENGTH * dir)
-		local hit, hitPoint, hitNormal = workspace:FindPartOnRayWithIgnoreList(ray, {LP.Character})
 
-		--DrawClass.Draw3D.Ray(ray.Origin, ray.Direction)
+		local hit, _, hitNormal =
+			workspace:FindPartOnRayWithIgnoreList(ray, { LP.Character or workspace })
 
-		if (hit) then
-			feelerNormalSum = feelerNormalSum + FEELER_WEIGHTING * angleWeight * hitNormal --* hitDistSqInv
-			feelerHitCount = feelerHitCount + 1
+		if hit then
+			feelerNormalSum += FEELER_WEIGHTING * angleWeight * hitNormal
+			feelerHitCount += 1
 		end
 	end
 
-	if (centerRayHitCount + downHitCount + feelerHitCount > 0) then
+	if (downHitCount + feelerHitCount) > 0 then
 		local normalSum = mainDownNormal + downRaySum + feelerNormalSum
-		if (normalSum ~= ZERO) then
-			return normalSum.unit
+		if normalSum.Magnitude > 0 then
+			return normalSum.Unit
 		end
 	end
 
 	return oldGravityUp
 end
+
 
 Controller.GetGravityUp = GetGravityUp
