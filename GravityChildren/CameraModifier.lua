9519465c792c5924cc1c5612e4639942fa0e479b
bignegro <charlene302@hotmail.com>
2025-12-24 19:27:07 -0500

    Update CameraModifier.lua

--- GravityChildren/CameraModifier.lua
+++ GravityChildren/CameraModifier.lua
@@ -5,13 +5,35 @@ local CameraModifier = {}
 CameraModifier.__index = CameraModifier
 
 function CameraModifier.new(player, controller)
+	local self = setmetatable({}, CameraModifier)
+
+	local playerModule = player.PlayerScripts:WaitForChild("PlayerModule")
+	local cameraModule = playerModule:WaitForChild("CameraModule")
+	local basecam = require(cameraModule:WaitForChild("BaseCamera"))
+
+	self.Controller = controller
+	self.BaseCamera = basecam
+
+	self._oldGetUpVector = basecam.GetUpVector
+	self._oldUpdateMouseBehavior = basecam.UpdateMouseBehavior
+
+	-- 🔒 ALWAYS LOCK MOUSE (required for free look)
+	function basecam.UpdateMouseBehavior(this)
+		UserGameSettings.RotationType = Enum.RotationType.MovementRelative
+		UIS.MouseBehavior = Enum.MouseBehavior.LockCenter
+	end
+
+	-- 🌍 THIS IS THE KEY: gravity-relative camera
+	function basecam.GetUpVector()
+		return controller.GravityUp
+	end
+
 	return self
 end
 
 function CameraModifier:Destroy()
-	self.BaseClass.Update = self.DefaultUpdate
-	self.BaseClass.GetUpVector = self.DefaultGetUpVector
-	self.BaseClass.UpdateMouseBehavior = self.DefaultMouseBehavior
+	self.BaseCamera.GetUpVector = self._oldGetUpVector
+	self.BaseCamera.UpdateMouseBehavior = self._oldUpdateMouseBehavior
 end
 
 return CameraModifier
