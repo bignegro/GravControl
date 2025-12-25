a7b66fb28856db36f8bec5fb314dade854b8da78
bignegro <charlene302@hotmail.com>
2025-12-24 19:17:32 -0500

    Enhance camera behavior with gravity-relative updates

--- GravityChildren/CameraModifier.lua
+++ GravityChildren/CameraModifier.lua
@@ -13,33 +13,53 @@ function CameraModifier.new(player, controller)
 
 	self.Controller = controller
 	self.BaseClass = basecam
-	self.DefaultMouseBehavior = basecam.UpdateMouseBehavior
-	self.DefaultGetUpVector = basecam.GetUpVector
 
-	-- Mouse behavior (do NOT touch yaw)
+	self.DefaultUpdate = basecam.Update
+	self.DefaultGetUpVector = basecam.GetUpVector
+	self.DefaultMouseBehavior = basecam.UpdateMouseBehavior
+
+	-- Mouse behavior (do NOT force shiftlock)
 	function basecam.UpdateMouseBehavior(this)
 		UserGameSettings.RotationType = Enum.RotationType.MovementRelative
-
-		if this.inFirstPerson or this.inMouseLockedMode then
-			UIS.MouseBehavior = Enum.MouseBehavior.LockCenter
-		elseif this.isRightMouseDown or this.isMiddleMouseDown then
-			UIS.MouseBehavior = Enum.MouseBehavior.LockCurrentPosition
-		else
-			UIS.MouseBehavior = Enum.MouseBehavior.Default
-		end
+		UIS.MouseBehavior = Enum.MouseBehavior.Default
 	end
 
-	-- 🔥 THIS IS THE KEY
+	-- Gravity-relative up
 	function basecam.GetUpVector()
 		return controller.GravityUp
+	end
+
+	-- 🔥 THE IMPORTANT PART: rotate yaw around GravityUp
+	function basecam.Update(this, dt)
+		-- Let Roblox do its normal camera math
+		self.DefaultUpdate(this, dt)
+
+		local cam = workspace.CurrentCamera
+		local up = controller.GravityUp
+
+		-- Rotate camera yaw into gravity space
+		local look = cam.CFrame.LookVector
+		local flatLook = look - look:Dot(up) * up
+
+		if flatLook.Magnitude > 0.001 then
+			flatLook = flatLook.Unit
+			local right = flatLook:Cross(up).Unit
+			cam.CFrame = CFrame.fromMatrix(
+				cam.CFrame.Position,
+				right,
+				up,
+				-flatLook
+			)
+		end
 	end
 
 	return self
 end
 
 function CameraModifier:Destroy()
-	self.BaseClass.UpdateMouseBehavior = self.DefaultMouseBehavior
+	self.BaseClass.Update = self.DefaultUpdate
 	self.BaseClass.GetUpVector = self.DefaultGetUpVector
+	self.BaseClass.UpdateMouseBehavior = self.DefaultMouseBehavior
 end
 
 return CameraModifier
